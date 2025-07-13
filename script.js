ce71b5ca648afe297efeb73ae53d4ca834885fae
Harone Magalhaes <harone15@outlook.com>
2025-07-12 21:03:49 -0300

    Primeiro commit do site da clínica

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.querySelectorAll('.link-button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert("Em breve essa função será ativada!");
+    });
+});
